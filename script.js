4358bdf373f637f2091698586ac3ceddbd75deaf
Hackrisque <toninrazafison@gmail.com>
2023-06-02 10:24:40 +0300

    Ajout de tous les fichiers

---
+++ script.js
@@ -0,0 +1,52 @@
+// exercice 1 - a
+
+// let a = prompt("Entrez la valeur de a") * 1;
+// let b = prompt("Entrez la valeur de b") * 1;
+let p1 = document.getElementById('algo1')
+let p4 = document.getElementById('algo4')
+
+// function sontInvOuOpp(a, b) {
+//     if (a + b === 0 || a * b === 1) {
+//         return true
+//     } else {
+    //         return false
+    //     }
+    // }
+    
+    // p1.innerHTML = sontInvOuOpp(a,b);
+    
+    // exercice 1 - b
+    
+            
+    // let p2 = document.getElementById('algo2')
+    // let T = [2, -2, 0.5]
+    // function existeInvOuOppConsecutifs(T) {
+        //     for (let i = 0; i < T.length - 1; i++) {
+//         if ([(T[i], T[i+1])]) {
+//             return true
+//         } else {
+    //             return false
+    //         }
+//     }
+// }
+
+// p2.innerHTML = existeInvOuOppConsecutifs(T)
+
+// exercice 1 - c
+
+// let p3 = document.getElementById('algo3')
+// let T = [2, -2, 0.5]
+
+// function existeInvOuOpp(T) {
+//     for (let i = 0; i < T.length-1; i++) {
+//         if ([(T[i] === 0)]) {
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+// }
+
+// p3.innerHTML = existeInvOuOpp(T)
+
+
